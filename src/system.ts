14fddb39c6513fb3b108184f643b8217b2b1b2a2
hieivip <54146330+hieivip@users.noreply.github.com>
2023-03-17 08:07:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/system.ts
@@ -0,0 +1,18 @@
+export const setting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "123456aA",
+  systemRule: ""
+}
+
+export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 由 [@hieivip](https://github.com/hieivip) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
+- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://chat.jorto.net/chatgpt.jpg)，但不接受任何付费功能请求。
+- API Key 由 jorto 提供 请合理使用
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof setting
+
+export const resetContinuousDialogue = false
